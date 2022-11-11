ad844fbb9b23cde38b06d667fcc1d648c48f44be
arios968 <116163896+arios968@users.noreply.github.com>
2022-11-10 21:06:22 -0500

    Add files via upload
    correccion en las alertas

--- node.js
+++ node.js
@@ -1,12 +1,66 @@
 
+const nodos = document.querySelectorAll(".contenedor-texto");
+const form = document.querySelector(".datos");
+
+let pepito = [];
+
+const datos = {
+  firstName: {
+    value: "",
+    name: "Nombre",
+  },
+  lastName: {
+    value: "",
+    name: "Apellido",
+  },
+  email: {
+    value: "",
+    name: "Correo",
+  },
+  password: {
+    value: "",
+    name: "Contraseña",
+  },
+};
+
+nodos.forEach((Input) => {
+  Input.addEventListener("keyup", (e) => {
+    datos[e.target.name].value = e.target.value.trim();
+  });
+});
+
+form.addEventListener("click", (e) => {
+  e.preventDefault();
+  let hasErrors = false;
+
+  for (const nodos in datos) {
+    const errorEl = document.getElementById(`error-${nodos}`);
+    errorEl.innerText = "";
+
+    if (datos[nodos].value === "") {
+      errorEl.innerText = `${datos[nodos].name} no puede estar vacío`;
+      hasErrors = true;
+    }
+    if (nodos === "email") {
+      const regex = new RegExp(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/);
+      if (regex.test(datos[nodos].value) === false) {
+        errorEl.innerText = "Parece que  no es un correo electronico ";
+        hasErrors = true;
+      }
+    }
+  }
+
+  if (hasErrors === false) {
+    pepito.push(datos);
+    localStorage.setItem("data", JSON.stringify(pepito));
+    console.log(pepito);
+  }
+});
 
 
 
 
-
-
-
-
+/*
 const alertas = document.querySelectorAll(".cuadros")
 const boton= document.querySelector(".boton")
 let pepito=JSON.parse(localStorage.getItem("data"))
@@ -49,7 +103,8 @@ boton.addEventListener("click", (e)=>{
     localStorage.setItem("data",JSON.stringify(arrayform))
     console.log(arrayform);
     
-})
+})*/
+
 
 
 
